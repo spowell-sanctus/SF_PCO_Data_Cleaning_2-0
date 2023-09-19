76216982b9e9a9b094e4c66da4bcc9c846175d22
Sarah Powell <spowell@sanctuschurch.com>
2023-09-19 10:35:28 -0400

    Prepping for Phase 1 Release

--- SF_create_id_households.py
+++ SF_create_id_households.py
@@ -7,7 +7,7 @@ from utils.helpers import get_now, convert_numbers_to_boolean, create_special_id
 
 ''' Write the rows of data for the final contacts with household associations file for import'''
 def loop_through_contacts_update_label(salesforce_import,salesforce_final_contact_household_import,primary_contact_id, household_id, household_name):
-                
+        # print(f'\n{primary_contact_id}\t{household_id}\t{household_name}')
         # create a file to recieve the updated information
         with open(salesforce_import, 'r', encoding = 'ISO-8859-1') as salesforce_import:
             # create the writer object           
@@ -15,14 +15,20 @@ def loop_through_contacts_update_label(salesforce_import,salesforce_final_contac
                 for index, row in enumerate(csv.reader(salesforce_import)):
                     # create holder for the row data to pull from the cleaned data
                     row_to_write = ''
-                    # if not the header row - that was created in write_final_sf_contact_households_header_row
+                    # if not the header row - that was created in write_final_sf_contact_households_header_row                    
                     if index != 0:
+                        # print(f'0: {row[0]}\t1: {row[1]}')
                         # write the row with the association label at 42 and the household id at 43, and the source at 44
-                        if row[1] == primary_contact_id and row[42] == '' and row[43] == '':
+                        # if the primary_contact_id from household matches the contact Id row, it's a primary contact
+                        if row[0] == primary_contact_id and row[42] == '' and row[43] == '':
+                            # print(f'1. {row}')
                             row_to_write = row+['Primary Household Contact']+[household_id]+['SF']
-                        elif row[0] == household_id and row[42] == '' and row[43] == '':
+                        # if the account id of the person matches the household ID must have a secondary member
+                        elif row[1] == household_id and row[42] == '' and row[43] == '':
+                            # print(f'2. {row}')
                             row_to_write = row+['Other Household Member']+[household_id]+['SF']
-                        
+                   
+                   
                     if row_to_write != '':
                         with open (salesforce_final_contact_household_import, 'a') as sf_final:
                                 writer = csv.writer(sf_final)
@@ -71,11 +77,15 @@ def create_sf_household_label(households_raw, households_final, salesforce_impor
             starttime = datetime.now()
             for index, row in enumerate(csv.reader(households_raw)):
                 
+                # data from households export
                 primary_contact_id = row[8]
                 household_name = row[1]
                 household_id = row[0]
+                
+                # if index == 0:
+                #     print(f'\n{primary_contact_id}\t{household_name}\t{household_id}')
                 if primary_contact_id != 'npe01__One2OneContact__c':
-                    
+                    # passing the data from the households export
                     loop_through_contacts_update_label(salesforce_import,salesforce_final_contact_household_import,primary_contact_id, household_id, household_name)
                     # print(f"running... {index}")
     
@@ -87,53 +97,53 @@ def create_sf_household_label(households_raw, households_final, salesforce_impor
 
 '''Cleans the data from Salesforce export for import into HubSpot contacts (no associations)'''
 def clean_salesforce_data(row):
-  
-    do_not_call = row[14]  
-    media_consent = row[41]
-    province_state = row[4]
-    membership_status = row[39]
-    is_deceased = row[29]  
-    do_not_contact = row[30]
-    email_optout = row[14] 
-    gifts = row[40]
-    epi_pen_carried = row[32]
+    
+    province_state = row[6]
+    email_optout = row[15] 
+    do_not_call = row[16] 
+    is_deceased = row[31]  
+    do_not_contact = row[32] 
+    epi_pen_carried = row[34]
+    membership_status = row[41]
+    gifts = row[42]
+    media_consent = row[43]
 
     # turn 0 into false, and 1 into true
-    row[41] = convert_numbers_to_boolean(media_consent)
-    row[14] = convert_numbers_to_boolean(do_not_call)
-    row[29] = convert_numbers_to_boolean(is_deceased)
-    row[30] = convert_numbers_to_boolean(do_not_contact)
-    row[14] = convert_numbers_to_boolean(email_optout)
-    row[32] = convert_numbers_to_boolean(epi_pen_carried)
+    row[15] = convert_numbers_to_boolean(email_optout)
+    row[16] = convert_numbers_to_boolean(do_not_call)
+    row[31] = convert_numbers_to_boolean(is_deceased)
+    row[32] = convert_numbers_to_boolean(do_not_contact)
+    row[34] = convert_numbers_to_boolean(epi_pen_carried)
+    row[43] = convert_numbers_to_boolean(media_consent)
    
     # if gifts include "Interpretation", update to "Interpretation of Tongues" to be read by dropdown; add a ';' to front of the list
     if gifts != '':
         gifts = gifts.replace('Interpretation', 'Interpretation of Tongues')        
         gifts = ';' + gifts
-        row[40] =  gifts
+        row[42] =  gifts
         
 
     # update SF Ontario mistakes
     sf_prov_mistakes = ['On', 'Ontario', '9', 'Canada', 'Ontario (ON)', 'ontario', 'Ontarip', 'ONT', 'Ont', 'OnON', 'Onterio','State (US Residents)*']
 
     if province_state in sf_prov_mistakes:
-        row[4] = 'ON'
+        row[6] = 'ON'
     elif province_state == 'Ajax':
-        row[3] = 'Ajax'
-        row[4] = 'ON'
+        row[5] = 'Ajax'
+        row[6] = 'ON'
     elif province_state == 'Peterborough':
-        row[3] = 'Peterborough'
-        row[4] = 'ON'
+        row[5] = 'Peterborough'
+        row[6] = 'ON'
     elif province_state == 'Ga':
-        row[4] = "GA"
+        row[6] = "GA"
     elif province_state == 'PEI':
-        row[4] = 'PE'
+        row[6] = 'PE'
     
     # update salesforce statuses
     if membership_status == 'Regular Attender':
-        row[39] = 'I attend'
+        row[41] = 'I attend'
     elif membership_status == 'Visitor':
-        row[39] = 'I am a guest'
+        row[41] = 'I am a guest'
 
 
 '''Handler to clean the data, create a special id, and write to a file'''
@@ -149,18 +159,28 @@ def update_contact_export(export_filename, updated_filename):
             for index, row in enumerate(csv.reader(csvinput)): 
                 # add emailId as first column
                 if index == 0:
+                    # do_not_call = row[16]  
+                    # media_consent = row[43]
+                    # province_state = row[6]
+                    # membership_status = row[41]
+                    # is_deceased = row[31]  
+                    # do_not_contact = row[32]
+                    # email_optout = row[16] 
+                    # gifts = row[42]
+                    # epi_pen_carried = row[34]
+                    # print(f'\n{do_not_call}\n{media_consent}\n{province_state}\n{membership_status}\n{is_deceased}\n{do_not_contact}\n{email_optout}\n{gifts}\n{epi_pen_carried}')
                     # add the necessary headers for the special id, association label, household id, and the import source of "SF"                       
-                    writer.writerow(['EmailID']+row + ['Association Label']+['Household Id']+['Import Source'])
+                    writer.writerow(row + ['Association Label']+['Household Id']+['Import Source'])
                     
                 # add special id for any subsequent rows after header
                 else:
                    
                     clean_salesforce_data(row)
   
-                    f_name = row[0]  
-                    l_name = row[1] 
-                    p_email = row[11] # col AF/32
-                    alt_email = row[15] # col BG/59
+                    f_name = row[2]  
+                    l_name = row[3] 
+                    p_email = row[13] # col AF/32
+                    alt_email = row[17] # col BG/59
 
                     # make special id
                     # special_id = create_special_id(f_name, l_name, p_email,'',alt_email)
